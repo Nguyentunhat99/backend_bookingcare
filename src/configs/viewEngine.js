94b329793a497d0b42ad50f85a2140ff0c0118c3
Nguyentunhat99 <nguyentunhat99@gmail.com>
2023-01-18 23:00:06 +0700

    #9 HTTP REQUEST METHOD & SỬ DỤNG DATABASE VỚI NODE.JS | Node.JS Cơ Bản Cho Beginners Từ A Đến Z

--- src/configs/viewEngine.js
+++ src/configs/viewEngine.js
@@ -1,9 +1,9 @@
 import express from "express";
-const path = require('path');
+// const path = require('path');
 
 const configViewEngine = (app) => {
-    // app.use(express.static('./src/public'));
-    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static('./src/public'));
+    // app.use(express.static(path.join(__dirname, 'public')));
     app.set("view engine", "ejs");
     app.set("views", "./src/views")
 }
