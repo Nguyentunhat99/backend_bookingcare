eee507a1b1f3781e39ce3aa297e844cb03cca06b
Nguyentunhat99 <nguyentunhat99@gmail.com>
2023-01-20 00:15:13 +0700

    HOÀN THIỆN CRUD - THÊM MỚI, HIỂN THỊ, CẬP NHẬT VÀ XÓA DỮ LIỆU

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -10,11 +10,58 @@ let getHomepage = async (req, res) => {
 let getDetailPage = async (req, res) => {
     let userid = req.params.userId;
     let [user, fields] = await connection.execute('SELECT * FROM users where id = ?', [userid]);
-    // console.log('check rq params:', req.params);
+    console.log('check rq params:', req.params);
     return res.send(JSON.stringify(user));
 }
 
+let createNewUser = async (req, res) => {
+    // console.log(req.body);
+    let { firstName, lastName, email, address } = req.body;//destructuring
+    //let firstName = req.body.firstName
+    //let lastName = req.body.lastName
+    //let email = req.body.email
+    //let address = req.body.address
+    await connection.execute('insert into users(firstName ,lastName ,email ,address) values(?, ?, ?, ?)'
+    ,[firstName, lastName, email, address])
+    return res.redirect('/');
+}
+
+let deleteUser = async (req, res) => {
+    let userid = req.params.userId;
+    // console.log(userid);
+    await connection.execute('delete from  users where id = ?', [userid]);
+    return res.redirect('/');
+}
+
+let editUser = async (req, res) => {
+    let userid = req.params.userId;
+    // console.log(userid);
+    let [userEdit, fields] = await connection.execute('SELECT * FROM users where id = ?', [userid]);
+    console.log(userEdit);
+    return res.render('update.ejs', { userEdit: userEdit });
+    // return res.send('ok');
+}
+
+let updateUser = async (req, res) => {
+    let {  firstName, lastName, email, address, id } = req.body;
+    console.log(req.body);
+    console.log(id);
+    console.log(firstName);
+    console.log(lastName);
+    console.log(email);
+    console.log(address);
+    await connection.execute('update users set firstName= ?, lastName = ? , email = ? , address= ? where id = ?',
+    [firstName, lastName, email, address, id]);
+    return res.redirect('/');
+    
+}
+
+
 module.exports = {
     getHomepage,
-    getDetailPage
+    getDetailPage,
+    createNewUser,
+    deleteUser,
+    editUser,
+    updateUser
 }
--- src/route/web.js
+++ src/route/web.js
@@ -5,7 +5,11 @@ let router = express.Router();
 
 const initWebRoute = (app) => {
     router.get('/',homeController.getHomepage);
-    router.get('/detail/user/:userId', homeController.getDetailPage)
+    router.get('/detail/user/:userId', homeController.getDetailPage);
+    router.post('/createNewUser', homeController.createNewUser);
+    router.get('/deleteUser/:userId', homeController.deleteUser);
+    router.get('/editUser/:userId', homeController.editUser);
+    router.post('/updateUser', homeController.updateUser);
     // router.get('/about', (req, res) => {
     //     res.send(`I'm Nhat!`)
     // })
